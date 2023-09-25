a0600f06c9a99dc9e0305c366895b64101c76de7
Ekushey Computer <majedulislam117@gmail.com>
2023-09-25 16:40:18 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+///This programme how to creat a stateful widget and how it work
 void main(){
   runApp(const MyApp());
 }
@@ -11,37 +14,99 @@ class MyApp extends StatelessWidget{
       home: HomePage(),
     );
   }
-
 }
 class HomePage extends StatefulWidget{
   const HomePage({super.key});
 
   @override
   State<StatefulWidget> createState() {
-  return MyHomepageUi();
+   return HomePageView();
   }
-
+}
+class HomePageView extends State<HomePage>{
+  var sum=0.0;
+Map<String,double>InputNum={
+  'Num1':0.0,
+  'Num2':0.0,
+  'Num3':0.0,
+};
+InputChange(Key,Value){
+  setState(() {
+    InputNum.update(Key, (value) => double.parse(Value));
+  });
+}
+  addValue(){
+     sum=InputNum['Num1']!+InputNum['Num2']!+InputNum['Num3']!;
   }
-class MyHomepageUi extends State<HomePage> {
-  int count=0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Counter App'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(count.toString(),style: TextStyle(fontSize: 30),),
-      ),
-        floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-    onPressed: (){
-          setState(() {
-            count+=1;
-          });
-          }
-        ),
-    );
+   return Scaffold(
+     appBar: AppBar(
+       title: Text('Sum App'),
+       centerTitle: true,
+     ),
+     body: Padding(
+       padding: EdgeInsets.all(40),
+       child: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+           children: [
+             Text('Sum=$sum',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+            SizedBox(height: 10,),
+            TextFormField(
+              onChanged: (value){
+                   InputChange('Num1', value);
+              },
+              decoration: InputDecoration(
+                label: Text('Enter first number'),
+                    border: OutlineInputBorder(),
+                fillColor: Colors.cyanAccent,
+                filled: true,
+              ),
+            ),
+             SizedBox(height: 10,),
+             TextFormField(
+               onChanged: (value){
+                 InputChange('Num2', value);
+               },
+               decoration: InputDecoration(
+               label: Text('Enter second number'),
+               border: OutlineInputBorder(),
+               fillColor: Colors.cyanAccent,
+               filled: true,
+             ),),
+             SizedBox(height: 10,),
+             TextFormField(
+               onChanged: (value){
+                 InputChange('Num3', value);
+               },
+               decoration: InputDecoration(
+               label: Text('Enter third number'),
+               border: OutlineInputBorder(),
+               fillColor: Colors.cyanAccent,
+               filled: true,
+             ),),
+             SizedBox(height: 10,),
+             Container(
+               width: double.infinity,
+               child: ElevatedButton(onPressed: (){
+                      addValue();
+               }, child: Text('Add'),
+               style: ElevatedButton.styleFrom(
+                 padding: EdgeInsets.all(22),
+                 backgroundColor: Colors.deepPurpleAccent,
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(10),
+                 ),
+
+               ),),
+             )
+           ],
+
+         ),
+       ),
+     ),
+   );
   }
+
 }
